4dbf980e8754cad236258f344976a22554d5114f
mymyiove <124661562+mymyiove@users.noreply.github.com>
2026-05-22 13:40:31 +0900

    Add files via upload

--- src/js/config.js
+++ src/js/config.js
@@ -1,1 +1,1 @@
-window.OneLearnConfig={defaultTenantId:"woongjin",testVerificationCode:"123456",termsVersion:"2026-05-14",privacyVersion:"2026-05-14"};
+window.OneLearnConfig=window.OneLearnConfig||{};
